134a42ce1f1d45ce4444e5c9e313855aba4efdc0
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-11 23:51:33 +0600

    eye button toast added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,13 +1,24 @@
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import toast, { Toaster } from 'react-hot-toast';
 import React from 'react';
 import './Quiz.css';
 
+
+
 const Quiz = ({quiz}) => {
     const {correctAnswer,question,id, options} = quiz;
+
+    
+    const rightAnswer = () => toast(correctAnswer);
     console.log(quiz);
     return (
         <div className='quiz-container'>
-            <h3>Quiz: {}{question}</h3>
-            
+            <div className='quiz-question'>
+                <h2>Question: {}{question}</h2>                         
+                <p onClick={rightAnswer}><FontAwesomeIcon  icon={faEye}></FontAwesomeIcon></p>
+                <Toaster />
+            </div>
             <div className='quiz-options'>
                 {
                     options.map(option => {
