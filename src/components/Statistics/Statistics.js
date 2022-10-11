754f1710e3c1f264f91445500cd4ff5b0c43ea11
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-11 10:55:13 +0600

    Chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, Tooltip, XAxis, YAxis,  Legend } from 'recharts';
+import './Statistics.css';
 
 const Statistics = () => {
+    const {data} = useLoaderData();
     return (
         <div>
-            <h3>this is statistics</h3>
+            <h2 className='graph-title'>Chart of Total Question of quizzes</h2>
+            <LineChart  className='graph' width={500} height={400} data={data}>
+                <Line type="monotone" dataKey='total' stroke="#82ca9d" />
+                <XAxis dataKey='name' />
+                <Tooltip/>
+                <YAxis />
+                <Legend />
+            </LineChart>
         </div>
     );
 };
