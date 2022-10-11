4e9640d1bf9bae4856528d3f62e68ad0b7ac65e3
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-11 23:13:42 +0600

    quiz design added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Quiz.css';
 
 const Quiz = ({quiz}) => {
     const {correctAnswer,question,id, options} = quiz;
     console.log(quiz);
     return (
-        <div>
+        <div className='quiz-container'>
             <h3>Quiz: {}{question}</h3>
-            <div>
+            
+            <div className='quiz-options'>
                 {
                     options.map(option => {
                         return(
--- src/components/Quizzes/Quizzes.js
+++ src/components/Quizzes/Quizzes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
+import './Quizzess.css';
 
 const Quizzes = () => {
     const quizzes = useLoaderData().data;
     const questions = quizzes.questions;
     
     return (
-        <div>
-            <h1>Quiz for {quizzes.name}</h1>
+        <div className='quizzes'>
+            <h1 className='quiz-header'>Quiz for {quizzes.name}</h1>
             <div>
               {
                 questions.map(quiz => <Quiz
